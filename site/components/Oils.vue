1054e700eaccb927951963f45d87722c753b5bf3
Gemma Black <gblackuk@googlemail.com>
2022-09-30 18:14:48 +0100

    fix: deduplicate oils grid

---
+++ site/components/Oils.vue
@@ -0,0 +1,114 @@
+<template>
+    <div>
+        <!-- Story -->
+        <ul class="df-m maxw-xxxsuper ml-au mr-au pl2 pr2 pt2-m pb0-m ff-inria-serif">
+        <li class="mr3-m w-4-12-m mb2 relative">
+            <div class="relative h-xbig">
+            <img
+                alt="A rosemary sprig"
+                class="w-100 absolute h-100 l0 t0 of-cover"
+                src="https://images.unsplash.com/photo-1517135399940-2855f5be7c4b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=40"
+            />
+            <span class="bg-dark absolute t0 l0 h-100 w-100 o-30"></span>
+            </div>
+            <p class="fs0 absolute light tfu t-50 l-50 tr-translate-50n center">Rosemary</p>
+        </li>
+        <li class="mr3-m mb2 w-4-12-m relative">
+            <div class="mb1 relative h-xbig">
+            <img
+                alt="5 whole almonds on a table"
+                class="w-100 absolute h-100 l0 t0 of-cover"
+                src="https://images.unsplash.com/photo-1535117156854-d5c5243361a8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=40"
+            />
+            <span class="bg-dark absolute t0 l0 h-100 w-100 o-30"></span>
+            </div>
+            <p class="fs0 absolute light tfu t-50 l-50 tr-translate-50n center">Almond</p>
+        </li>
+        <li class="mb2 w-4-12-m relative">
+            <div class="mb1 relative h-xbig">
+            <img
+                alt="A water melon with a slice cut out"
+                class="w-100 absolute h-100 l0 t0 of-cover"
+                src="https://images.unsplash.com/photo-1582281298055-e25b84a30b0b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=40"
+            />
+            <span class="bg-dark absolute t0 l0 h-100 w-100 o-30"></span>
+            </div>
+            <p class="fs0 absolute light tfu t-50 l-50 tr-translate-50n center">Melon seed</p>
+        </li>
+        </ul>
+
+        <ul class="df-m maxw-xxxsuper ml-au mr-au pl2 pr2 pt0-m pb2-m ff-inria-serif">
+        <li class="mr3-m w-4-12-m mb2 relative">
+            <div class="relative h-xbig">
+            <img
+                alt="Lush green tropical hills"
+                class="w-100 absolute h-100 l0 t0 of-cover"
+                src="https://images.unsplash.com/photo-1527017200692-2feca35f864f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=40"
+            />
+            <span class="bg-dark absolute t0 l0 h-100 w-100 o-30"></span>
+            </div>
+            <p class="fs0 absolute light tfu t-50 l-50 tr-translate-50n center">Castor</p>
+        </li>
+        <li class="mr3-m mb2 w-4-12-m relative">
+            <div class="mb1 relative h-xbig">
+            <img
+                alt="A purple flower"
+                class="w-100 absolute h-100 l0 t0 of-cover"
+                src="https://images.unsplash.com/photo-1586632690015-248015e3a4e6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=40"
+            />
+            <span class="bg-dark absolute t0 l0 h-100 w-100 o-30"></span>
+            </div>
+            <p class="fs0 absolute light tfu t-50 l-50 tr-translate-50n center">Rose</p>
+        </li>
+        <li class="mb2 w-4-12-m relative">
+            <div class="mb1 relative h-xbig">
+            <img
+                alt="A green field with mountains in the distance"
+                class="w-100 absolute h-100 l0 t0 of-cover"
+                src="https://images.unsplash.com/photo-1438786657495-640937046d18?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=40"
+            />
+            <span class="bg-dark absolute t0 l0 h-100 w-100 o-30"></span>
+            </div>
+            <p class="fs0 absolute light tfu t-50 l-50 tr-translate-50n center">Abyssinian</p>
+        </li>
+        </ul>
+
+        <ul class="df-m maxw-xxxsuper ml-au mr-au pl2 pr2 pt0-m pb2-m ff-inria-serif">
+        <li class="mr3-m w-4-12-m mb2 relative">
+            <div class="relative h-xbig">
+            <img
+                alt="An olive plant"
+                class="w-100 absolute h-100 l0 t0 of-cover"
+                src="https://images.unsplash.com/photo-1543872980-ac54e8f41d5a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=40"
+            />
+            <span class="bg-dark absolute t0 l0 h-100 w-100 o-30"></span>
+            </div>
+            <p class="fs0 absolute light tfu t-50 l-50 tr-translate-50n center">Olive</p>
+        </li>
+        <li class="mr3-m mb2 w-4-12-m relative">
+            <div class="mb1 relative h-xbig">
+            <img
+                alt="Baobab trees either side of a solitary road with a single pedestrian"
+                class="w-100 absolute h-100 l0 t0 of-cover"
+                src="https://images.unsplash.com/photo-1570742544137-3a469196c32b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=40"
+            />
+            <span class="bg-dark absolute t0 l0 h-100 w-100 o-30"></span>
+            </div>
+            <p class="fs0 absolute light tfu t-50 l-50 tr-translate-50n center">Baobab</p>
+        </li>
+        <li class="mb2 w-4-12-m relative">
+            <a href="/guides/tea-tree-oil-hair-care">
+            <div class="mb1 relative h-xbig">
+                <img
+                alt="Rolling lush hills"
+                class="w-100 absolute h-100 l0 t0 of-cover"
+                src="https://images.unsplash.com/photo-1470087167738-6aa485ff65dc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=40"
+                />
+                <span class="bg-dark absolute t0 l0 h-100 w-100 o-30"></span>
+            </div>
+            <p class="fs0 absolute light tfu t-50 l-50 tr-translate-50n center">Tea Tree</p>
+            </a>
+        </li>
+        </ul>
+    </div>
+</template>
